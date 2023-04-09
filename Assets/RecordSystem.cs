1f571416bb43a7eef5d45df55f466388edf6e483
Jackycyc <Jackychiu02@gmail.com>
2023-04-09 15:33:44 +0800

    。

--- Assets/RecordSystem.cs
+++ Assets/RecordSystem.cs
@@ -6,22 +6,15 @@ using UnityEngine.UI;
 
 public class RecordSystem : MonoBehaviour
 {
-    public DataManager dm;
     public Text winnerName;
     public Text loserName;
     public Text deathReason;
     public Text date;
 
-    private void Awake() {
-        dm = GameObject.Find("DataManager").GetComponent<DataManager>();
-    }
-    public void DisplayRecord(){
-        for(int i = 0 ;i < dm.myRecordList.record.Count; i++){
-            winnerName.text = dm.myRecordList.record[i].winnerName;
-            loserName.text = dm.myRecordList.record[i].loserName;
-            deathReason.text = dm.myRecordList.record[i].reason;
-            date.text = dm.myRecordList.record[i].date;
-        }
-        
+    public void DisplayRecord(string RwinnerName, string RloserName, string Rreason, string Rdate){
+        winnerName.text = RwinnerName;
+        loserName.text = RloserName;
+        deathReason.text = Rreason;
+        date.text = Rdate;
     }
 }
--- Assets/dataManager.cs
+++ Assets/dataManager.cs
@@ -27,29 +27,66 @@ public class DataManager : MonoBehaviour
 
     public RecordList myRecordList = new RecordList();
 
-    void Start() {
-        ReadCSV(); 
-        filename = Application.dataPath + "/Resources/TestCSV.csv";   
-        AddRecord("Hi","Bye","grenade","1/1/1");
-        
+    private void Update()
+    {
+        GameObject[] gosc;
+        gosc = GameObject.FindGameObjectsWithTag("cutscene");
+        if (gosc.Length > 0)
+        {
+            Debug.Log("cutScene");
+            UpdateRecordPanel();
+        }
     }
 
-    public void ReadCSV(){
+
+    public void UpdateRecordPanel()
+    {
+        GameObject[] record;
+        record = GameObject.FindGameObjectsWithTag("record");
+        if (myRecordList.record.Count < 4)
+        {
+            for(int j = 0;j < 2; j++){
+
+            }
+            for (int i = 0; i < myRecordList.record.Count; i++)
+            {
+                GameObject newRecord = Instantiate<GameObject>(Resources.Load("records") as GameObject);
+                newRecord.GetComponent<RecordSystem>().DisplayRecord(myRecordList.record[i].winnerName,
+                                                                    myRecordList.record[i].loserName,
+                                                                    myRecordList.record[i].reason,
+                                                                    myRecordList.record[i].date);
+                newRecord.transform.parent = gameObject.transform;
+            }
+        }
+    }
+
+    void Start()
+    {
+        ReadCSV();
+        filename = Application.dataPath + "/Resources/TestCSV.csv";
+        AddRecord("Hi", "Bye", "grenade", "1/1/1");
+
+    }
+
+    public void ReadCSV()
+    {
         textAssetData = Resources.Load<TextAsset>("TestCSV");
-        string[] data = textAssetData.text.Split(new string[] { ",", "\n"}, System.StringSplitOptions.None);
-        int tableSize = data.Length/4 - 1;
-        for(int i =0; i < tableSize; i++){
+        string[] data = textAssetData.text.Split(new string[] { ",", "\n" }, System.StringSplitOptions.None);
+        int tableSize = data.Length / 4 - 1;
+        for (int i = 0; i < tableSize; i++)
+        {
             myRecordList.record.Add(new Record());
-            myRecordList.record[i].winnerName = data[5*(i+1)];
-            myRecordList.record[i].loserName = data[5*(i+1) +1];
-            myRecordList.record[i].reason = data[5*(i+1) +2];
+            myRecordList.record[i].winnerName = data[5 * (i + 1)];
+            myRecordList.record[i].loserName = data[5 * (i + 1) + 1];
+            myRecordList.record[i].reason = data[5 * (i + 1) + 2];
             // myRecordList.record[i].duration = int.Parse(data[5*(i+1) +3]);
-            myRecordList.record[i].date = data[5*(i+1) +4];
+            myRecordList.record[i].date = data[5 * (i + 1) + 4];
         }
 
     }
 
-    public void AddRecord(string winnerName, string loserName, string reasonOfDeath, string date){
+    public void AddRecord(string winnerName, string loserName, string reasonOfDeath, string date)
+    {
         Record newRecord = new Record();
         newRecord.winnerName = winnerName;
         newRecord.loserName = loserName;
@@ -59,9 +96,10 @@ public class DataManager : MonoBehaviour
         WriteCSV(newRecord);
     }
 
-    public void WriteCSV(Record newRecord){
+    public void WriteCSV(Record newRecord)
+    {
         TextWriter textWriter = new StreamWriter(filename, true);
         textWriter.WriteLine(newRecord.winnerName + "," + newRecord.loserName + "," + newRecord.reason + "," + newRecord.date);
         textWriter.Close();
-    }
+    }
 }
