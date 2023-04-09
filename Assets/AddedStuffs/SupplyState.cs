a16fd0dd88fca91466ea2a34ba22f58d5bef3a90
KevsFwe <u3564232@connect.hku.hk>
2023-04-09 17:48:25 +0800

    SolvedManyBugs

--- Assets/AddedStuffs/SupplyState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SupplyState : MonoBehaviour
-{
-    public int supply01 = 1;
-    public int supply02 = 1;
-}
--- Assets/jsond.cs
+++
@@ -1,5 +0,0 @@
-[System.Serializable]
-public class jsond
-{
-    public string playername;
-}
