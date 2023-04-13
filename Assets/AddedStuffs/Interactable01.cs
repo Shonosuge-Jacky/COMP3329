85bcc36abca34882bf21e817b155e02cf730c570
amywyp <amywyp@connect.hku.hk>
2023-04-13 17:33:00 +0800

    add spawn stuff again

--- Assets/AddedStuffs/Interactable01.cs
+++ Assets/AddedStuffs/Interactable01.cs
@@ -5,17 +5,69 @@ using Photon.Pun;
 
 public class Interactable01 : MonoBehaviourPunCallbacks
 {
-    public GameObject theSupply;
     public GameObject Object001;
-    public void active()
-    {   
-        photonView.RPC("activerpc",RpcTarget.All);
+    public GameObject effect001;
+    public bool opened;
+    private float effectCountdown; // For effect duration
+
+    void Start()
+    {
+        opened = false;
+        effectCountdown = 15f;
     }
 
-    [PunRPC] 
+    void Update()
+    {
+        effectCountdown -= Time.deltaTime;
+
+        if (effectCountdown <= 0f && effect001.active == true) // Disable effect after 15 seconds
+        {
+            effect001.active = false;
+        }
+
+        if (opened && !this.gameObject.GetComponent<Animation>().IsPlaying("Crate_Open"))
+        {
+            if (photonView.IsMine)
+            {
+                PhotonNetwork.Destroy(this.gameObject);
+                Debug.Log("Coor: "+transform.position+" destroyed");
+                opened = false;
+            }
+        }
+
+        // --- For fading out crate ---
+        // *** Have not found solution to rendering problem: if set material as transparent/fade, can see through
+
+        // if (opened)        // If crate is opened, fade out
+        // {
+        //     var objRenderer = Object001.GetComponentInChildren<MeshRenderer>();
+        //     var supplyRenderer = this.gameObject.GetComponentInChildren<MeshRenderer>();
+        //     float speed = 0.3f;
+        //     Color modifiedColor = objRenderer.material.color;
+        //     modifiedColor.a -= speed * Time.deltaTime;
+        //     objRenderer.material.color = modifiedColor;
+        //     supplyRenderer.material.color = modifiedColor;
+        //     if (modifiedColor.a <= 0.0f) // When transparent, destroy object
+        //     {
+        //         Destroy(this.gameObject);
+        //         Destroy(Object001);
+        //         Destroy(effect001);
+        //         Debug.Log("destroyed");
+        //         opened = false;
+        //     }
+        // }
+    }
+
+    public void active()
+    {
+        photonView.RPC("activerpc", RpcTarget.All);
+    }
+
+    [PunRPC]
     public void activerpc()
-    {   
-        Object001.GetComponent<MeshCollider>().enabled=true;
-        theSupply.GetComponent<Animation>().Play("Crate_Open");
+    {
+        Object001.GetComponent<MeshCollider>().enabled = true;
+        this.gameObject.GetComponent<Animation>().Play("Crate_Open");
+        opened = true;
     }
 }
