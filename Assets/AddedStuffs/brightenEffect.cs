cd436bb803044fe02d24a535958b604634f18016
yuenkwan <78483446+jojochan09@users.noreply.github.com>
2023-04-17 14:06:05 +0800

    adjust the chose grenade effect

--- Assets/AddedStuffs/brightenEffect.cs
+++ Assets/AddedStuffs/brightenEffect.cs
@@ -10,27 +10,27 @@ public class brightenEffect : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-       if (photonView.IsMine)
+        if (photonView.IsMine)
         {
             if (Input.GetKey("q"))
             {
-                transform.GetComponent<RectTransform>().localPosition = new Vector3(-880f, -461.9f, 0);
+                transform.GetComponent<RectTransform>().localPosition = new Vector3(-880f, -445.9f, 10f);
             }
             if (Input.GetKey("1"))
             {
-                transform.GetComponent<RectTransform>().localPosition = new Vector3(-780f, -461.9f, 0);
+                transform.GetComponent<RectTransform>().localPosition = new Vector3(-780f, -445.9f, 10f);
             }
             if (Input.GetKey("2"))
             {
-                transform.GetComponent<RectTransform>().localPosition = new Vector3(-680f, -461.9f, 0);
+                transform.GetComponent<RectTransform>().localPosition = new Vector3(-680f, -445.9f, 10f);
             }
             if (Input.GetKey("3"))
             {
-                transform.GetComponent<RectTransform>().localPosition = new Vector3(-580f, -461.9f, 0);
+                transform.GetComponent<RectTransform>().localPosition = new Vector3(-580f, -445.9f, 10f);
             }
             if (Input.GetKey("e"))
             {
-                transform.GetComponent<RectTransform>().localPosition = new Vector3(-480f, -461.9f, 0);
+                transform.GetComponent<RectTransform>().localPosition = new Vector3(-480f, -445.9f, 10f);
             }
         }
     }
